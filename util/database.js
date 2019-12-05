5dc90e3775842241eef2d1cf5cfdfc7f8d48d98d
Amanda Golubics <amanda.golubics@gmail.com>
2019-12-05 00:59:22 -0800

    Changes to database

--- util/database.js
+++ util/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'us-cdbr-iron-east-05.cleardb.net',
-    user: 'bd5d14a58293b1',
-    database: 'heroku_f8ae03c68f3c3f8',
-    password: '42ade8d3'
+    user: 'b2f58bb9274bd6',
+    database: 'heroku_8e0dd26144ae870',
+    password: '304c5184'
 })
 
 module.exports = pool.promise();
